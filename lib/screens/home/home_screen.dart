982f30283232169c4cce1eb3bd872e1809211ed7
gorkembayraktar <myemail>
2024-03-12 15:43:58 +0300

    unnecessary

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -105,65 +105,6 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget columnItems(context, SwipeLoaded state){
-      return Column(
-        children: [
-          Draggable(
-            child: UserCard(
-              user: state.users.first,
-            ),
-            feedback: UserCard(
-              user: state.users.first,
-            ),
-            childWhenDragging: UserCard(
-              user: state.users[1],
-            ),
-            onDragEnd: (drag){
-              if(drag.velocity.pixelsPerSecond.dx < 0){
-                context.read<SwipeBloc>()..add(SwipeLeftEvent(user: state.users[0]));
 
-                print('swiped left');
-              }else{
-                context.read<SwipeBloc>()..add(SwipeRightEvent(user: state.users[0]));
-
-                print('swiped right');
-              }
-            },
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 60),
-            child:  Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ChoiceButton(
-                  width: 60,
-                  height: 60,
-                  size: 25,
-                  iconData: Icons.clear_rounded,
-                  color: Colors.redAccent,
-                  hasGradient: false,
-                ),
-                ChoiceButton(
-                  width: 80,
-                  height: 80,
-                  size: 30,
-                  iconData: Icons.favorite,
-                  color: Colors.white,
-                  hasGradient: true,
-                ),
-                ChoiceButton(
-                  width: 60,
-                  height: 60,
-                  size: 25,
-                  iconData: Icons.watch_later,
-                  color: Colors.black,
-                  hasGradient: false,
-                )
-              ],
-            ),
-          ),
-        ],
-      );
-  }
 }
 
