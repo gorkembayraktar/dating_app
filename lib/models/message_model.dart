7e139b5fed26fce52417a85292e83e09d508eb0f
gorkembayraktar <myemail>
2024-03-13 17:12:07 +0300

    model ve fake data init islemi yapıldı

---
+++ lib/models/message_model.dart
@@ -0,0 +1,68 @@
+import 'package:equatable/equatable.dart';
+import 'package:intl/intl.dart';
+
+class Message extends Equatable{
+  final int id;
+  final int senderId;
+  final int receiverId;
+  final String message;
+  final DateTime dateTime;
+  final String timeString;
+
+  Message({
+    required this.id,
+    required this.senderId,
+    required this.receiverId,
+    required this.message,
+    required this.dateTime,
+    required this.timeString
+  });
+
+  @override
+  List<Object?> get props => [id, senderId, receiverId, message, dateTime, timeString];
+
+
+  static List<Message> messages = [
+      Message(
+        id: 1,
+        senderId: 1,
+        receiverId: 2,
+        message: 'Merhaba, nasılsın?',
+        dateTime: DateTime.now(),
+        timeString: DateFormat.jm().format(DateTime.now())
+      ),
+      Message(
+          id: 2,
+          senderId: 2,
+          receiverId: 1,
+          message: 'İyiyim, teşekkürler.',
+          dateTime: DateTime.now(),
+          timeString: DateFormat.jm().format(DateTime.now())
+      ),
+      Message(
+          id: 3,
+          senderId: 1,
+          receiverId: 2,
+          message: 'Bende iyiyim, teşekkürler.',
+          dateTime: DateTime.now(),
+          timeString: DateFormat.jm().format(DateTime.now())
+      ),
+      Message(
+          id: 4,
+          senderId: 1,
+          receiverId: 3,
+          message: 'merhaba nasılsın?',
+          dateTime: DateTime.now(),
+          timeString: DateFormat.jm().format(DateTime.now())
+      ),
+      Message(
+          id: 5,
+          senderId: 3,
+          receiverId: 1,
+          message: 'teşekkürler iyiyim.',
+          dateTime: DateTime.now(),
+          timeString: DateFormat.jm().format(DateTime.now())
+      )
+  ];
+
+}
---
+++ lib/models/user_match_model.dart
@@ -0,0 +1,39 @@
+import 'package:dating_app/models/models.dart';
+import 'package:equatable/equatable.dart';
+
+class UserMatch extends Equatable{
+  final int id;
+  final int userId;
+  final User matchedUser;
+  final List<Chat>? chat;
+
+  const UserMatch({
+      required this.id,
+      required this.userId,
+      required this.matchedUser,
+      required this.chat
+  });
+  @override
+  List<Object?> get props => [id, userId, matchedUser];
+
+
+  static List<UserMatch> matches = [
+      UserMatch(
+          id: 1,
+          userId: 1,
+          matchedUser: User.users[1],
+          chat: Chat.chats.where(
+                  (chat) => chat.userId == 1 && chat.matchedUserId == 2
+          ).toList()
+      ),
+      UserMatch(
+          id: 2,
+          userId: 1,
+          matchedUser: User.users[2],
+          chat: Chat.chats.where(
+                  (chat) => chat.userId == 1 && chat.matchedUserId == 3
+          ).toList()
+      )
+  ];
+
+}
